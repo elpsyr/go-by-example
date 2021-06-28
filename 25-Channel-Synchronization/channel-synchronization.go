07ae9d114a6bb53005b52d9ec7b3f6e73f53e3a6
he11c <13735599246@163.com>
2021-06-28 15:52:26 +0800

    add channel

---
+++ 25-Channel-Synchronization/channel-synchronization.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func worker(done chan bool) {
+	fmt.Print("working....")
+	time.Sleep(time.Second * 10)
+	fmt.Println("done")
+	done <- true
+}
+
+func main() {
+	done := make(chan bool, 1)
+	go worker(done)
+
+	//程序将一直阻塞，直至收到 worker 使用通道发送的通知
+	fmt.Println(<-done)
+}
